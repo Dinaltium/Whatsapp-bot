bb34163a739981243059e748f5894e5897153e75
Dinaltium <orneryraptor778@gmail.com>
2026-05-22 22:20:09 +0530

    perf: cache Puppeteer downloaded browser inside node_modules to speed up Render builds

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,6 @@ const { join } = require("path");
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+  // Changes the cache location for Puppeteer to be within node_modules so it is cached by Render.
+  cacheDirectory: join(__dirname, "node_modules", ".cache", "puppeteer"),
 };
