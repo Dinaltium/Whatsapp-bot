70ec624ef298a8bbce675d197fe524322f2c7b91
Dinaltium <orneryraptor778@gmail.com>
2026-05-31 16:18:03 +0530

    refactor(agents): migrate EmbedclubAgent to BotHandler interface

--- agents/EmbedclubAgent.ts
+++ agents/EmbedclubAgent.ts
@@ -1,5 +1,15 @@
+import { BotHandler, BotContext, AgentResult } from "./core/BotHandler";
 import { handleMessage } from "./ECB/handler";
+import { ECB_HELP_TEXT } from "./ECB/intro";
 
-export default {
-  handleMessage,
+const EmbedclubAgent: BotHandler = {
+  botId: 1,
+  name: "ECB",
+  requiresAllowlist: true as const,
+  isInDomain: (_prompt) => true,
+  handleMessage: (ctx: BotContext): Promise<AgentResult> =>
+    handleMessage(ctx.session, ctx.prompt),
+  getHelpText: () => ECB_HELP_TEXT,
 };
+
+export default EmbedclubAgent;
