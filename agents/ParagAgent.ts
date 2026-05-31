60f12326433bc58cd0ffb983b8caba7ec54dfa7f
Dinaltium <orneryraptor778@gmail.com>
2026-05-31 16:17:49 +0530

    refactor(agents): migrate ParagAgent to BotHandler interface

--- agents/ParagAgent.ts
+++ agents/ParagAgent.ts
@@ -1,5 +1,21 @@
+import { BotHandler, BotContext, AgentResult } from "./core/BotHandler";
 import { handleMessage } from "./PARAG/handler";
+import { PARAG_HELP_TEXT } from "./PARAG/intro";
 
-export default {
-  handleMessage,
+const ParagAgent: BotHandler = {
+  botId: 0,
+  name: "PARAG",
+  requiresAllowlist: true as const,
+  isInDomain: (_prompt) => true, // PARAG handles all prompts, domain restriction is inside handler
+  handleMessage: (ctx: BotContext): Promise<AgentResult> =>
+    handleMessage(
+      ctx.session,
+      ctx.prompt,
+      ctx.groqApiKey,
+      ctx.groqModel,
+      ctx.isAdmin,
+    ),
+  getHelpText: () => PARAG_HELP_TEXT,
 };
+
+export default ParagAgent;
