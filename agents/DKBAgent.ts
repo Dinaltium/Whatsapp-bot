55e9eeb5cf8c2165d55d8f8c0c7069124edc8ba1
Dinaltium <orneryraptor778@gmail.com>
2026-05-31 16:18:19 +0530

    refactor(agents): migrate DKBAgent to BotHandler interface

--- agents/DKBAgent.ts
+++ agents/DKBAgent.ts
@@ -1,7 +1,24 @@
+import { BotHandler, BotContext, AgentResult } from "./core/BotHandler";
 import { handleMessage } from "./DKB/handler";
 import { classifyAndAutoAddMentor } from "../services/DKB/mentorService";
+import { DKB_HELP_TEXT } from "./DKB/intro";
 
-export default {
-  handleMessage,
-  classifyAndAutoAddMentor,
+const DKBAgent: BotHandler = {
+  botId: 2,
+  name: "DKB",
+  requiresAllowlist: true as const,
+  isInDomain: (_prompt) => true,
+  handleMessage: (ctx: BotContext): Promise<AgentResult> =>
+    handleMessage(
+      ctx.session,
+      ctx.prompt,
+      ctx.groqApiKey,
+      ctx.groqModel,
+      ctx.isAdmin,
+      ctx.senderJid,
+    ),
+  getHelpText: () => DKB_HELP_TEXT,
 };
+
+export { classifyAndAutoAddMentor };
+export default DKBAgent;
